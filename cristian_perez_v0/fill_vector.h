877523c85eff80f369b5bf5bbc7de35ac6f67393
ACRCris <crperezp@unal.edu.co>
2022-05-14 14:40:02 -0500

    delete unless files

--- cristian_perez_v0/fill_vector.h
+++
@@ -1,4 +0,0 @@
-#include <vector>
-#pragma once
-std::vector<int> fillVector(int N, int seed, double p );
-std::string toString(std::vector<int> grid);
--- cristian_perez_v0/propuesta_clases/main_precolation.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "nodo.h"
-
-int main(){
-  Nodo n = Nodo(0);
-  n.setState(1);
-  return 0;
-}
