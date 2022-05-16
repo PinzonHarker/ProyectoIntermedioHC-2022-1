3233edd0758a66ba1e58eede850eeb728f50a3a8
Andrés Pinzón Harker <apinzonh@unal.edu.co>
2022-05-16 01:12:11 -0500

    add *.x to gitignore. Makefile reformated. There's still some troubles for debug and valgrind

--- tests/percolation_test.cpp
+++ tests/percolation_test.cpp
@@ -1,12 +1,13 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch2/catch.hpp"
 
 //#include "catch.hpp"
 
-#include "Fill.h"
+#include "../libs/Fill.h"
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-  
+TEST_CASE("Factorials are computed", "[factorial]")
+{
+
   Fill f;
   REQUIRE(f.percolate(39, 0.4, 3) == false);
   REQUIRE(f.percolate(2, 0.5, 7) == true);
