1a517b485d2d909a6f65ada7b3bb3a9c6d18cda0
jgsuarezc <jgsuarezc@unal.edu.co>
2022-05-18 13:11:59 -0500

    propuesta estadisticas 8

--- src/Stats.cpp
+++ src/Stats.cpp
@@ -1,26 +1,28 @@
 
 include "..\libs\Fill.h"
 include <vector>
+include <math
 
-int seed =9;
-double p =0.1;
-void N=32;
+
 void main (void){
+  int seed =9;
+  double p =0.1;
+  void N=32;
 
 double counter =0.1;
 
 //para tamaños de matrices n=32,64,128...
 //varia el tamaño de la matriz
 
-for(int kk=1;ii==4;jj++){
+for(int ii=1;ii==4;ii++){
   N=2*N;
 
 std::vector<auto> TC (30,0);//guarda el tamaño de los clusters en un vector
 
-for (int ii=0 ;ii<30;ii++){
+for (int jj=0 ;jj<30;jj++){
 
 //indica el tamaño del cluster percolante en funcion de un tamaño n de la matriz
-//esta mal porque ese no es el tamaño del cluster
+//esta mal porque ese no es el tamaño del cluster poner algo
   TC[ii] = fill.getPercolation().numberOfOpenSites();//guarda el tamaño del cluster
 
 }
@@ -30,10 +32,15 @@ suma=suma+ T[tt];
 }
 // promedio cluster para un tamaño  fijo
 auto promedio =suma/30
+//vector guarda la desviacion estandar para N fijo
 std::vector<int> desviacion(30,0);
-for(int )
+for (int tt; tt<30;tt++){
+T[tt]=sqrt(std::pow((TC[ii]-promedio),2)/2;
+}
 
-
+for (int tt; tt<30;tt++){
+std::cout << N*N << TC[jj]<<desviacion[jj]<<'\n';;
+}
 
 
 }
