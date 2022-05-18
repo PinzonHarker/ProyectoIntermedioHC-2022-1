0a9dc33b693973c18645e10245ca36b580ede19b
jgsuarezc <jgsuarezc@unal.edu.co>
2022-05-18 12:11:25 -0500

    propuesta estadisticas 6

--- src/Stats.cpp
+++ src/Stats.cpp
@@ -9,21 +9,23 @@ void main (void){
 double counter =0.1;
 
 //para tamaños de matrices n=32,64,128...
+//varia el tamaño de la matriz
 for(int kk=1;ii==4;jj++){
   N=2*N;
-for (int ii=0 ;ii==512;ii++){
-  //indica el tamaño del cluster percolante en funcion de un tamaño n de la matriz
-
+  auto  X =0;// variable guardara que suma el tamaño de los cluster para calcular  promedio del cluster promedio para una mtriz fija
+//varia la probabilidad entre (0,1)
+for (int ii=0 ;ii==30;ii++){
+//indica el tamaño del cluster percolante en funcion de un tamaño n de la matriz
 //esta mal porque ese no es el tamaño del cluster
   auto tamaño= fill.getPercolation().numberOfOpenSites();//guarda el tamaño del cluster
-
-
+// suma el tamano del cluster para un tamaño fijo
+  P=P+fill.getPercolation().numberOfOpenSites();
   std::cout <<N*N<< (tamaño/N) << '\n';
 
 }
 
+auto promedio =X/30;
 
 
-std::std::cout <<'\t';
 }
 }
