cf6a8b8adfbd2b7639d1ec6140fe8d37b47ae02e
jgsuarezc <jgsuarezc@unal.edu.co>
2022-05-18 10:50:50 -0500

    propuesta estadisticas

---
+++ src/Stats.cpp
@@ -0,0 +1,16 @@
+
+include "..\libs\Fill.h"
+
+int seed =9978797665;
+double s =0.1;
+void N=10;
+void main (void){
+
+double counter =0.1;
+
+//
+for (int ii=1 ;ii<1;ii=30){
+
+s=10;
+  std::cout << fill.percolate(seed, p, N) << '\n';
+}
